cba6a6c41c11cdf162916c88c44882c48c899ca2
Yojhan-Alexis <54258838+Yojhan-Alexis@users.noreply.github.com>
2021-02-20 16:24:11 -0500

    servidor
    xd

---
+++ Express/src/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+
+const app = express();
+
+app.use(cors());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(morgan('dev'));
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log("localhost:" + PORT);
+})
